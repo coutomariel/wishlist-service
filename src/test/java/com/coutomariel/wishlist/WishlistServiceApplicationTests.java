b86693e408f9a240fbcb5d86c09abd069925ebb0
mariel <coutomariel@gmail.com>
2023-08-25 23:17:41 -0300

    Initial commit

---
+++ src/test/java/com/coutomariel/wishlist/WishlistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coutomariel.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
